74fca354bfe99cd58578636d79dd07cc6f61b3c8
Sunil Gupta <64741835+HackGuru108@users.noreply.github.com>
2022-08-31 02:56:43 +0100

    Add files via upload

--- Programs/files.py
+++ Programs/files.py
@@ -1,28 +1,28 @@
-
-
+from traceback import print_tb
 import requests
 import threading
 
-def find_files(url_input,domain):
+def findfiles(Host):
+    print("\n====================================================================\n")
+    print(f'following are the Files Directory present on {Host} \n')
+    def find_files(url_input,domain):
     
-    try:
-        global request
-        request = requests.get(f"http://{url_input}/{domain}",timeout=0.4)
-    except:
-        pass
+        try:
+            global request
+            request = requests.get(f"http://{url_input}/{domain}",timeout=0.4)
+            url = f"{request}"
     
-    url = f"{request}"
-    
-    server_response_code = int(url[11:14])
+            server_response_code = int(url[11:14])
 
-    if 200<=server_response_code<=299:
-        print(f"found: {domain}")
-    
+            if 200<=server_response_code<=299:
+                print(f"found: {domain}")
+        except:
+            pass
 
-url_input = input("Url: ")
+    url_input = Host
 
-domains = open('domains.txt','r')
+    domains = open('domains.txt','r')
 
-for domain in domains:
-    thread = threading.Thread(target=find_files, args = (url_input,domain,))
-    find_files(url_input,domain)    
+    for domain in domains:
+        thread = threading.Thread(target=find_files, args = (url_input,domain,))
+        find_files(url_input,domain) 
